0d58398e4b2a6927218536fb71dca680359efebd
zhwang <wangzhihua2017@qq.com>
2017-08-01 13:56:07 +0800

    home欢迎页，list列表页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import home from '@/components/home/home.vue'
+import list from '@/components/list/list.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: list
     }
   ]
 })
