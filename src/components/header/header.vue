2ac6fac9d793bc66864cc9448d4f5cbba877594c
zhwang <wangzhihua2017@qq.com>
2017-08-01 14:39:04 +0800

    增加header页面

---
+++ src/components/header/header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="header">
+    <img src="./nav_icon.png" @click="menuShow">
+    <h1>title</h1>
+    <span></span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    methods: {
+      menuShow() {
+        alert('菜单显示')
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .header
+    position: fixed
+    top: 0
+    left: 0
+    width: 100%
+    height: 44px
+    background: #ffffff
+    border-bottom: 1px solid #e8e8e8
+    box-shadow: 0 0 4px rgb(0, 0, 0, .25)
+    transition: 0.3s
+    z-index: 9
+
+    h1
+      height: 100%
+      width: 100%
+      line-height 44px
+      font-size: 16px
+      text-align: center
+    img
+      position: absolute
+      left: 0
+      top: 0
+      width: 19px
+      height 16px
+      padding: 14px 15px
+</style>
--- src/components/list/list.vue
+++ src/components/list/list.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>111</div>
+  <div>
+    <v-header></v-header>
+    <div>111</div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import vHeader from '../header/header.vue'
 
+  export default {
+    components: {
+      vHeader
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" >
